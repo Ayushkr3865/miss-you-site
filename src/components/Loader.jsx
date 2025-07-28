d8627afba91c6983f461fe6ff51467498d5183a5
Ayushkr3865 <mushing672@gmail.com>
2025-07-29 00:59:47 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -153,7 +153,7 @@ export default function Loader() {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 2 }}
                 >
-                    My heart beats for you...
+                    Do you even know how badly I am missing you🥺...
                 </motion.p>
 
                 <motion.div
